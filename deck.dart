4679cb715d992e848c593acb57e1b7ac3c876288
Dulio Denis <dulio.denis@yahoo.com>
2018-07-05 19:44:41 -0400

    Implemented a deck constructor

--- deck.dart
+++ deck.dart
@@ -1,13 +1,39 @@
 // Deck Program
+// -----------------------------------------------------------------------------
 class Deck {
-  List<Card> cards;
+  List<Card> cards = [];
+
+  var suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+  var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  Deck() {
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
+
+  toString() {
+    int i =1;
+    String returnString = "";
+    for (var card in cards) {
+      returnString = returnString + "${i} ${card.rank} of ${card.suit}\n";
+      i++;
+    }
+    return returnString;
+  }
 }
 
 class Card {
   String rank;
   String suit;
+
+  Card(this.rank, this.suit);
 }
 
 main() {
-  
+  var deck = Deck();
+  print(deck);
 }
